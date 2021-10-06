bc55f22322e3c1e88e4fd8c0075db5aa0a2a931d
arseniiarsenii <arseniivelichko2@gmail.com>
2021-10-06 17:03:46 +0300

    improved IPFS connection logic

--- src/io_gateway/ipfs.py
+++ src/io_gateway/ipfs.py
@@ -5,13 +5,31 @@ from loguru import logger
 
 from ..shared.config import config
 
-IPFS_CLIENT: ipfshttpclient.Client = ipfshttpclient.connect()
+
+def _get_ipfs_client() -> tp.Optional[ipfshttpclient.Client]:
+    if not config.ipfs.enable:
+        logger.warning("IPFS capabilities are disabled in config-file")
+        return None
+
+    try:
+        return ipfshttpclient.connect()
+    except Exception as e:
+        logger.error(f"An error occurred while getting IPFS client: {e}")
+        return None
+
+
+IPFS_CLIENT: tp.Optional[ipfshttpclient.Client] = _get_ipfs_client()
 
 
 def publish_to_ipfs(file_path: str) -> tp.Tuple[str, str]:
     """publish file on IPFS"""
     logger.info(f"Publishing file {file_path} to IPFS")
-    result = IPFS_CLIENT.add(file_path)
+    client = IPFS_CLIENT or _get_ipfs_client()
+
+    if client is None:
+        raise ConnectionError(f"Connection to IPFS node failed, cannot publish file {file_path}")
+
+    result = client.add(file_path)
     ipfs_hash: str = result["Hash"]
     ipfs_link: str = config.ipfs.gateway_address + ipfs_hash
     logger.info(f"File {file_path} published to IPFS, hash: {ipfs_hash}")
