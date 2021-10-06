4a0d87c57ec6d78d610d6a73e396fcf70d9ba0ba
arseniiarsenii <arseniivelichko2@gmail.com>
2021-10-06 16:53:55 +0300

    minor improvements to the IO gateway

--- src/io_gateway/ipfs.py
+++ src/io_gateway/ipfs.py
@@ -5,12 +5,13 @@ from loguru import logger
 
 from ..shared.config import config
 
+IPFS_CLIENT: ipfshttpclient.Client = ipfshttpclient.connect()
+
 
 def publish_to_ipfs(file_path: str) -> tp.Tuple[str, str]:
     """publish file on IPFS"""
     logger.info(f"Publishing file {file_path} to IPFS")
-    ipfs_client: ipfshttpclient.Client = ipfshttpclient.connect()
-    result = ipfs_client.add(file_path)
+    result = IPFS_CLIENT.add(file_path)
     ipfs_hash: str = result["Hash"]
     ipfs_link: str = config.ipfs.gateway_address + ipfs_hash
     logger.info(f"File {file_path} published to IPFS, hash: {ipfs_hash}")
