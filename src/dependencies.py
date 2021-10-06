9e6209ce288ae9f0bb60fe57a2a73f665a0ad26c
arseniiarsenii <arseniivelichko2@gmail.com>
2021-10-06 16:28:42 +0300

    tweaks to authentication system

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,22 +1,19 @@
-from fastapi import HTTPException, status, Request
+from fastapi import HTTPException, Header, status
 
 from .database import MongoDbWrapper
 from .models import Employee
 from .shared.config import config
 
 
-async def authenticate(request: Request) -> Employee:
+async def authenticate(rfid_card_id: str = Header(...)) -> Employee:
     try:
-        rfid_card_id: str = request.headers.get("rfid_card_id")
-        user: Employee = await MongoDbWrapper().get_concrete_employee(rfid_card_id)
-
-        if user.rfid_card_id == "1111111111" and config.api_server.production_environment:
+        if rfid_card_id == "1111111111" and config.api_server.production_environment:
             raise ValueError("Development credentials are not allowed in production environment")
+
+        return await MongoDbWrapper().get_concrete_employee(rfid_card_id)
 
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=f"Authentication failed: {e}",
         )
-
-    return user
