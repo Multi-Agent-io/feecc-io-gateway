e0e3df1a10b98075c1a2b3ddfe2a5c3a03ba0127
arseniiarsenii <arseniivelichko2@gmail.com>
2021-10-12 13:02:11 +0300

    containerization improvements

--- src/io_gateway/ipfs.py
+++ src/io_gateway/ipfs.py
@@ -1,29 +1,38 @@
 import os
 import typing as tp
+from time import sleep
 
 import ipfshttpclient
 from loguru import logger
 
 from ..shared.config import config
 
-IS_DOCKERIZED: bool = os.environ.get("IS_DOCKERIZED", False)
+IS_DOCKERIZED: bool = bool(os.environ.get("IS_DOCKERIZED", False))
+logger.info(f"App {'is' if IS_DOCKERIZED else 'is not'} running in a containerized environment")
 
 
+@logger.catch
 def _get_ipfs_client() -> tp.Optional[ipfshttpclient.Client]:
 
     if not config.ipfs.enable:
         logger.warning("IPFS capabilities are disabled in config-file")
         return None
 
-    try:
-        node_address = f"/dns/{'ipfs-node' if IS_DOCKERIZED else 'localhost'}/tcp/5001/http"
-        client = ipfshttpclient.connect(addr=node_address)
-        logger.info(f"Successfully connected to the IPFS node at {node_address}")
-        return client
+    retry_delay = 5
+    for i in range(5):
+        try:
+            node_address = f"/dns/{'ipfs' if IS_DOCKERIZED else 'localhost'}/tcp/5001/http"
+            client = ipfshttpclient.connect(addr=node_address)
+            logger.info(f"Successfully connected to the IPFS node at {node_address}")
+            return client
 
-    except Exception as e:
-        logger.error(f"An error occurred while getting IPFS client: {e}")
-        return None
+        except Exception as e:
+            logger.error(f"An error occurred while getting IPFS client: {e}")
+            logger.warning(f"Attempt {i + 1} failed. Retrying in {retry_delay} s.")
+            sleep(retry_delay)
+
+    logger.warning("Retry attempts count exceeded. Connection to an IPFS node could not be established.")
+    return None
 
 
 IPFS_CLIENT: tp.Optional[ipfshttpclient.Client] = _get_ipfs_client()
