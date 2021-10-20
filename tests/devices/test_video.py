141436d7e9193312f0cbc32a044537a1a8a7c7f5
Nikolas <timofeevnik41@gmail.com>
2021-10-20 19:57:09 +0300

    :zap: tests architecture

---
+++ tests/devices/test_video.py
@@ -0,0 +1,29 @@
+from .. import test_client
+
+
+def test_get_video_cameras_list() -> None:  # TODO
+    pass
+
+
+def test_get_video_records_list() -> None:  # TODO
+    pass
+
+
+def test_start_first_record() -> None:  # TODO
+    pass
+
+
+def test_stop_first_record() -> None:  # TODO
+    pass
+
+
+def test_stop_first_record_again() -> None:  # TODO
+    pass
+
+
+def test_start_second_record() -> None:  # TODO
+    pass
+
+
+def test_stop_first_and_second_record() -> None:  # TODO
+    pass
