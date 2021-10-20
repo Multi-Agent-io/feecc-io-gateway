555f1249ce39049f155d86f9aa2a82b33311d53a
Nikolas <timofeevnik41@gmail.com>
2021-10-20 22:26:41 +0300

    :zap: external io gateway tests

--- tests/external_io/test_external_io.py
+++ tests/external_io/test_external_io.py
@@ -1,17 +1,52 @@
-from .. import test_client
+from .. import test_client, pytest_plugins
+import os
+
+test_filename = "ipfs_test.txt"
 
 
-def test_ipfs_push_invalid_path() -> None:  # TODO
-    pass
+def test_ipfs_push_nothing() -> None:
+    resp = test_client.post("/io-gateway/ipfs")
+    assert resp.status_code != 200, "/ipfs accepted literally nothing"
 
 
-def test_ipfs_push_valid_path() -> None:  # TODO
-    pass
+def test_ipfs_push_invalid_path() -> None:
+    req_data = {"filename": "wrong_file.name"}
+    resp = test_client.post("/io-gateway/ipfs", json=req_data)
+    assert resp.ok is True
+    assert resp.status_code != 200, "Gateway accepted invalid path"
 
 
-def test_pinata_push_invalid_path() -> None:  # TODO
-    pass
+def test_ipfs_push_valid_path() -> None:
+    with open(test_filename, "w") as f:
+        f.write("test file")
+    req_data = {"filename": test_filename}
+    resp = test_client.post("/io-gateway/ipfs", json=req_data)
+    if not os.path.exists(test_filename):
+        raise ValueError("Unable to create or delete file")
+    os.remove(test_filename)
+    assert resp.ok is True
+    assert resp.status_code == 200, f"File wasn't sent: {resp.json()}"
 
 
-def test_pinata_push_valid_path() -> None:  # TODO
-    pass
+def test_pinata_push_nothing() -> None:
+    resp = test_client.post("/io-gateway/pinata")
+    assert resp.status_code != 200, "/pinata accepted literally nothing"
+
+
+def test_pinata_push_invalid_path() -> None:
+    req_data = {"filename": "wrong_file.name"}
+    resp = test_client.post("/io-gateway/pinata", json=req_data)
+    assert resp.ok is True
+    assert resp.status_code != 200, "Gateway accepted invalid path"
+
+
+def test_pinata_push_valid_path() -> None:
+    with open(test_filename, "w") as f:
+        f.write("test file")
+    req_data = {"filename": test_filename}
+    resp = test_client.post("/io-gateway/pinata", json=req_data)
+    if not os.path.exists(test_filename):
+        raise ValueError("Unable to create or delete file")
+    os.remove(test_filename)
+    assert resp.ok is True
+    assert resp.status_code == 200, f"File wasn't sent: {resp.json()}"
