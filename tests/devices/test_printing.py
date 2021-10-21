0ab3948ba18a168ec56bfa85b11bf22c5d4ceaab
Nikolas <timofeevnik41@gmail.com>
2021-10-22 00:44:52 +0300

    :zap: endpoint name hotfix

--- tests/devices/test_printing.py
+++ tests/devices/test_printing.py
@@ -1,9 +1,34 @@
-from .. import test_client
+import os
+
+import pytest
+import os
+import requests
+
+from .. import test_client, pytest_plugins
 
 
-def test_print_image() -> None:  # TODO
-    pass
+@pytest.fixture
+def test_img() -> str:
+    test_picture = "test_img.svg"
+    if not os.path.exists(test_picture):
+        robonomics_logo = "https://robonomics.network/assets/static/robonomics-logo.4520fc0.674fd3b96847876764f360539d019573.svg"
+        logo = requests.get(robonomics_logo)
+        with open(test_picture, "wb") as f:
+            f.write(logo.content)
+    return test_picture
 
 
-def test_print_image_annotated() -> None:  # TODO
-    pass
+@pytest.mark.printer
+def test_print_image(test_img) -> None:
+    """FIXME: check behaviour"""
+    resp = test_client.post("/printing/print_image", files={"image_file": open(test_img, "rb")})
+    assert resp.ok is True
+    assert resp.status_code == 200
+
+
+@pytest.mark.printer
+def test_print_image_annotated(test_img) -> None:
+    resp = test_client.post("/printing/print_image", files={"image_file": open(test_img, "rb")},
+                            json={"annotation": "image with annotation"})
+    assert resp.ok is True
+    assert resp.status_code == 200
