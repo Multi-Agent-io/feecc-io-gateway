7eaf2972882671a71407f02df18bc2b76c0708b1
Nikolas <timofeevnik41@gmail.com>
2021-10-22 01:07:07 +0300

    :zap: video tests half-done

--- tests/devices/test_video.py
+++ tests/devices/test_video.py
@@ -1,29 +1,46 @@
-from .. import test_client
+import pytest
+
+from .. import test_client, pytest_plugins
 
 
-def test_get_video_cameras_list() -> None:  # TODO
-    pass
+@pytest.mark.video
+def test_get_video_cameras_list() -> None:
+    resp = test_client.get("/video/cameras")
+    assert resp.ok is True, resp.text
+    assert resp.json().get("cameras", None) is not None, "No cameras found (check config)"
 
 
-def test_get_video_records_list() -> None:  # TODO
-    pass
+@pytest.mark.video
+def test_get_video_records_list() -> None:
+    resp = test_client.get("/video/records")
+    assert resp.ok is True, resp.text
+    assert resp.json().get("ongoing_records", None) is None, "when did we started recording?"
+    assert resp.json().get("ended_records", None) is None, "when did we ended recording?"
 
 
+@pytest.mark.video
 def test_start_first_record() -> None:  # TODO
-    pass
+    resp = test_client.post("/video/camera/1/start")
+    assert resp.ok is True, resp.text
+    assert resp.status_code == 200, resp.json().get("details", None)
 
 
+
+@pytest.mark.video
 def test_stop_first_record() -> None:  # TODO
     pass
 
 
+@pytest.mark.video
 def test_stop_first_record_again() -> None:  # TODO
     pass
 
 
+@pytest.mark.video
 def test_start_second_record() -> None:  # TODO
     pass
 
 
+@pytest.mark.video
 def test_stop_first_and_second_record() -> None:  # TODO
     pass
