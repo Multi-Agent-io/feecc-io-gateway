16ac616376bc1ebfec4af6eba89822c787909c60
Nikolas <timofeevnik41@gmail.com>
2021-10-22 13:32:51 +0300

    :zap: cameras/recordings testing

--- tests/devices/test_video.py
+++ tests/devices/test_video.py
@@ -1,6 +1,13 @@
+import os
+import time
+import typing as tp
 import pytest
 
 from .. import test_client, pytest_plugins
+
+# Use TESTS_DELAY environ var to control time.sleep length. 2 seconds by default
+delay = os.environ.get("TESTS_DELAY", 2)
+tests_cache: tp.Dict[str, str] = {}
 
 
 @pytest.mark.video
@@ -24,23 +31,74 @@ def test_start_first_record() -> None:  # TODO
     assert resp.ok is True, resp.text
     assert resp.status_code == 200, resp.json().get("details", None)
 
+    tests_cache["first_rec"] = resp.json().get("record_id")
+
+    records_list_resp = test_client.get("/video/records")
+    assert len(records_list_resp.json().get("ongoing_records")) != 0, "recording hasn't started"
 
 
 @pytest.mark.video
-def test_stop_first_record() -> None:  # TODO
-    pass
+def test_stop_first_record() -> None:
+    resp = test_client.post("/video/camera/1/stop", json={"record_id": tests_cache["first_rec"]})
+    assert resp.ok is True, resp.text
+    assert resp.status_code == 200, resp.json().get("details", None)
+
+    records_list_resp = test_client.get("/video/records")
+    assert len(records_list_resp.json().get("ongoing_records")) == 0, "recording hasn't stopped"
 
 
 @pytest.mark.video
-def test_stop_first_record_again() -> None:  # TODO
-    pass
+def test_stop_first_record_again() -> None:
+    resp = test_client.post("/video/camera/1/stop", json={"record_id": tests_cache["first_rec"]})
+    assert resp.ok is True, resp.text
+    assert resp.status_code == 500, f"stopped nonexistent record : {resp.json().get('details', None)}"
 
 
 @pytest.mark.video
-def test_start_second_record() -> None:  # TODO
-    pass
+def test_start_second_record() -> None:
+    resp = test_client.post("/video/camera/1/start")
+    assert resp.ok is True, resp.text
+    assert resp.status_code == 200, resp.json().get("details", None)
+
+    tests_cache["second_rec"] = resp.json().get("record_id")
+
+    records_list_resp = test_client.get("/video/records")
+    assert len(
+        records_list_resp.json().get("ongoing_records")) != 0, "second recording hasn't started"
+    assert len(
+        records_list_resp.json().get("ended_records")) == 1, "second recording hasn't started"
 
 
 @pytest.mark.video
-def test_stop_first_and_second_record() -> None:  # TODO
-    pass
+def test_stop_second_record() -> None:
+    resp = test_client.post("/video/camera/1/stop", json={"record_id": tests_cache["second_rec"]})
+    assert resp.ok is True, resp.text
+    assert resp.status_code == 200, resp.json().get("details", None)
+
+    records_list_resp = test_client.get("/video/records")
+    assert len(records_list_resp.json().get("ongoing_records")) == 0, "recording hasn't stopped"
+    assert len(
+        records_list_resp.json().get("ended_records")) == 2, "second recording hasn't started"
+
+
+@pytest.mark.video
+def test_multiple_records_cycle() -> None:
+    first_rec_resp = test_client.post("/video/camera/1/start")
+    second_rec_resp = test_client.post("/video/camera/1/start")
+
+    time.sleep(delay)
+
+    records_list_resp = test_client.get("/video/records")
+    assert len(records_list_resp.json().get("ongoing_records")) == 2, "recordings hasn't started"
+
+    end_second_rec_resp = test_client.post("/video/camera/1/stop", json={
+        "record_id": first_rec_resp.json().get("record_id")})
+
+    time.sleep(delay)
+
+    end_first_rec_resp = test_client.post("/video/camera/1/stop", json={
+        "record_id": second_rec_resp.json().get("record_id")})
+
+    assert end_second_rec_resp.ok is True, end_second_rec_resp.text
+    assert end_first_rec_resp.ok is True, end_first_rec_resp.text
+    assert end_first_rec_resp.status_code == end_second_rec_resp.status_code, "An error occurred while trying to stop record 1 or 2"
